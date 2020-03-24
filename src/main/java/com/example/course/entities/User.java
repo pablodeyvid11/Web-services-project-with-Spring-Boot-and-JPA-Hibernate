07aba5bd1483a7ab373d9d0e4f9bc5886900665a
Pablo Deyvid <pablodeyvid11@gmail.com>
2020-03-24 16:19:38 -0300

    Order, Instant, ISO 8601 - PART 1

--- src/main/java/com/example/course/entities/User.java
+++ src/main/java/com/example/course/entities/User.java
@@ -1,13 +1,18 @@
 package com.example.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
-@Entity //diz que é uma entidade do banco de dados
+@Entity // diz que é uma entidade do banco de dados
+@Table(name = "tb_User")
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -18,6 +23,11 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+
+	@OneToMany(mappedBy = "client") // A lista virou uma chave extrangeira
+	// mappedBy = "client" : significa que client é como a instancia dessa classe é
+	// chamada em cada objeto da lista
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -68,6 +78,10 @@ public class User implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 
 	@Override
