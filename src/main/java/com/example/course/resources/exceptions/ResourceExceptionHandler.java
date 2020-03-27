00d67104015266b7fd14e111533eeafcec73736b
Pablo Deyvid <pablodeyvid11@gmail.com>
2020-03-27 00:00:49 -0300

    Exception handling - delete

--- src/main/java/com/example/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/course/resources/exceptions/ResourceExceptionHandler.java
@@ -9,16 +9,27 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
-@ControllerAdvice // pega as excessoes que acontecem para fazer uma ação 
+@ControllerAdvice // pega as excessoes que acontecem para fazer uma ação
 public class ResourceExceptionHandler {
-	
-	@ExceptionHandler(ResourceNotFoundException.class)//Para que ela seja capaz de interceptar a excessao
-	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request){
+
+	@ExceptionHandler(ResourceNotFoundException.class) // Para que ela seja capaz de interceptar a excessao
+	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found";
 		HttpStatus status = HttpStatus.NOT_FOUND;
-		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(DatabaseException.class) // Para que ela seja capaz de interceptar a excessao
+	public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+		String error = "Data base erros";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
 }
--- src/main/java/com/example/course/services/UserService.java
+++ src/main/java/com/example/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.example.course.entities.User;
 import com.example.course.repositories.UserRepository;
+import com.example.course.services.exceptions.DatabaseException;
 import com.example.course.services.exceptions.ResourceNotFoundException;
 
 //@Component //Registra a classe como componente do Spring, fazendo com que ela seja injetada automáticamente
@@ -29,11 +32,17 @@ public class UserService {
 	public User insert(User obj) {
 		return userRepository.save(obj);
 	}
-	
+
 	public void delete(Long id) {
-		userRepository.deleteById(id);
+		try {
+			userRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
-	
+
 	public User update(Long id, User obj) {
 		User entity = userRepository.getOne(id);
 		updateData(entity, obj);
@@ -43,6 +52,6 @@ public class UserService {
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());  
+		entity.setPhone(obj.getPhone());
 	}
 }
